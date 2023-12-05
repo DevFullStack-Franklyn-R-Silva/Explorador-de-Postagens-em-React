684186eeeca3de529ac38bbc8a1200b1f6119324
Franklyn Roberto da Silva <franklyn1998@live.com>
2023-12-05 14:17:31 -0300

    State com arrays e objetos

--- src/App.js
+++ src/App.js
@@ -1,70 +1,28 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.handlePClicks = this.handlePClicks.bind(this);
-    this.state = {
-      name: "Franklyn Roberto",
-      counter: 0,
-    };
-  }
-
-  handlePClicks() {
-    this.setState({ name: "Silva" });
-  }
-
-  handleAClicks = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
+  state = {
+    posts: [
+      { id: 1, title: "O título 1", body: "O corpo 1" },
+      { id: 2, title: "O título 2", body: "O corpo 2" },
+      { id: 3, title: "O título 3", body: "O corpo 3" },
+    ],
   };
-
   render() {
-    const { name, counter } = this.state;
+    const { posts } = this.state;
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClicks}>
-            {name} {counter}
-          </p>
-          <a
-            onClick={this.handleAClicks}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Este é o Link
-          </a>
-        </header>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body} </p>
+          </div>
+        ))}
       </div>
     );
   }
 }
-// function App() {
-// return (
-//   <div className="App">
-//     <header className="App-header">
-//       <img src={logo} className="App-logo" alt="logo" />
-//       <p>
-//         Edit <code>src/App.js</code> and save to reload.
-//       </p>
-//       <a
-//         className="App-link"
-//         href="https://reactjs.org"
-//         target="_blank"
-//         rel="noopener noreferrer"
-//       >
-//         Learn React
-//       </a>
-//     </header>
-//   </div>
-// );
-// }
 
 export default App;
